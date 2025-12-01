ff25ff8f414f199df7d2b8b7fa653bc505c6dbdb
Rolando-12 <rola.vargas@duocuc.cl>
2025-12-01 18:27:42 -0300

    Configuracion para pagar 20.0

--- Ventas/Ventas/src/main/java/com/example/Ventas/VentasApplication.java
+++ Ventas/Ventas/src/main/java/com/example/Ventas/VentasApplication.java
@@ -2,6 +2,9 @@ package com.example.Ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class VentasApplication {
@@ -10,4 +13,16 @@ public class VentasApplication {
 		SpringApplication.run(VentasApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*") // Permite TODOS los orígenes (incluyendo tu puerto 5174)
+						.allowedMethods("*") // Permite TODOS los métodos (GET, POST, etc.)
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
