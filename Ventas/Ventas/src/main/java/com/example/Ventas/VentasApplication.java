3dc1d623ecc2438cee398849dc71aa6910101046
Rolando-12 <rola.vargas@duocuc.cl>
2025-12-02 19:23:24 -0300

    arreglo de application

--- Ventas/Ventas/src/main/java/com/example/Ventas/VentasApplication.java
+++ Ventas/Ventas/src/main/java/com/example/Ventas/VentasApplication.java
@@ -2,27 +2,12 @@ package com.example.Ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class VentasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VentasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VentasApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*") 
-						.allowedMethods("*") 
-						.allowedHeaders("*");
-			}
-		};
-	}
 }
